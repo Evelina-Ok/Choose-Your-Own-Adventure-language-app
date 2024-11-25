a51a1a718f7329a5802cfcd9531a6f34753e8033
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-11-25 13:55:37 +0000

    #1 listen button still needs styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { textToSpeech } from "./utils/textToSpeech";
+import { ListenBttn } from "./components/ButtonListen/ButtonListen"
 
 function App() {
   const [text, setText] = useState<string>("");
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <>
+    <ListenBttn />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/ButtonListen/ButtonListen.tsx
@@ -0,0 +1,9 @@
+export function ListenBttn() {
+  return (
+    <button>
+      <div className=" w-12 rounded-full bg-yellow-500">
+        <span className="icon-[material-symbols--select-to-speak-outline-rounded]"></span>
+      </div>
+    </button>
+  );
+}
