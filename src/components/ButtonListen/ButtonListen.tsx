3bd0a755e27627e2d867c5af4a0eac7dd60c8c7e
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-11-26 10:30:53 +0000

    #3 made small changes in styling

--- src/components/ButtonListen/ButtonListen.tsx
+++ src/components/ButtonListen/ButtonListen.tsx
@@ -1,9 +1,7 @@
 export function ListenBttn() {
   return (
-    <button className="w-[80px] h-[80px] rounded-full bg-yellow-500 p-2.5" type="button">
-      <div className="">
-        <span className="size-[2rem] icon-[material-symbols--select-to-speak-outline-rounded]"></span>
-      </div>
+    <button className="w-[70px] h-[70px] pt-3.5 rounded-full justify-center bg-amber-500 p-2.5" type="button">
+        <span className="size-[2.5rem] bg-white justify-center icon-[material-symbols--select-to-speak-outline-rounded]"></span>
     </button>
   );
 }
