71ba41f2d6d7e0a6373cb2c26fcff526f3817107
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-11-29 15:50:09 +0000

    cleaned up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-
 import "./App.css";
-
-
 import { StoryProvider } from "./providers/storyProvider";
-import { ChooseAdventureGame } from "./components/chooseAdventureGame";
 import { LandingPage } from "./Pages/LandingPage/LandingPage";
-import { FlagPage } from "./Pages/FlagPage/FlagPage";
 
 
 function App() {
