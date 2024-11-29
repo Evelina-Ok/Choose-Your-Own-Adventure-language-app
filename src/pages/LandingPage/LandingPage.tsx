b52085a001e58a019f29a1c6470b616457750728
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-11-29 14:47:45 +0000

    #changed method of state if opened first time or not

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -2,8 +2,6 @@ import  Button from "../../components/button"
 import { useState } from "react";
 
 export function LandingPage() {
-    const [text, setText] = useState<string>("");
-    const [firstTime, setFirstTime]= useState(false)
   
   
     return (
@@ -12,11 +10,11 @@ export function LandingPage() {
       <div className="text-center text-white">
         <h1 className="my-40 mb-36 text-8xl font-bold font-['Amatic_SC']">TALK TO ME</h1>    
 
-    {!firstTime ? <><Button text="CONTINUE"/>
+    <Button text="CONTINUE"/>
 
         <Button text="CHAPTERS"/>
-        <Button text="START GAME IN ANOTHER LANGUAGE"/></>: 
-        <Button text="START"/>}
+        <Button text="START GAME IN ANOTHER LANGUAGE"/>
+        <Button text="START"/>
       </div>
     </div>
     )
