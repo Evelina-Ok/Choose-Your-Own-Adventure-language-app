7c2921587ccac03ed9ebec10bcd3653c16a81685
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-12-02 14:01:24 +0000

    #fixed errors in components & defined the story theme in  prompt

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import "./App.css";
 import { StoryProvider } from "./providers/storyProvider";
 import { LandingPage } from "./Pages/LandingPage/LandingPage";
 import { ChooseAdventureGame } from "./components/chooseAdventureGame";
+import Microphone from "./components/microphone";
+import BackArrow from "./components/BackArrow";
+import { ListenBttn } from "./components/ButtonListen/ButtonListen";
 
 function App() {
   return (
     <StoryProvider>
+     
       <ChooseAdventureGame />
     </StoryProvider>
   );
--- src/components/microphone.tsx
+++ src/components/microphone.tsx
@@ -1,24 +1,18 @@
 import MicrophoneIcon from 'src/assets/Microphone.svg'; 
 
 interface MicrophoneProps {
-  onClick?: () => void; 
+  onClick: () => void; 
 }
 
-const Microphone: React.FC<MicrophoneProps> = ({ onClick }) => {
-  const handleMicrophoneClick = () => {
-    if (onClick) {
-      onClick();
-   
-    }
-  };
+const Microphone = ({ onClick }:MicrophoneProps) => {
 
   return (
     <button
-      onClick={handleMicrophoneClick}
+      onClick={onClick}
       className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-600 hover:bg-blue-700 text-white p-4 rounded-full shadow-lg "
       aria-label="Activate Microphone"
     >
-      <img src={MicrophoneIcon} alt="Microphone Icon" className="w-6 h-6" />
+      <img src={'src/assets/Microphone.svg'} alt="Microphone Icon" className="w-6 h-6" />
     </button>
   );
 };
