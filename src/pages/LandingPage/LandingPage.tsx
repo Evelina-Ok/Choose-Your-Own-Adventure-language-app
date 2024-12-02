e3da387a7631d288b6a692ec26df3a9c95132a65
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-12-02 16:07:41 +0000

    #added navlink in flag page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,21 +1,47 @@
-import  Button from "../../components/button"
+import Button from "../../components/button";
 import { useState } from "react";
-
+import { Link, NavLink } from "react-router-dom";
+import { useStory } from "../../providers/storyProvider";
 export function LandingPage() {
-  
-  
-    return (
-      
+  const { story } = useStory();
+  return (
     <div className="bg-gray-700 flex justify-center h-screen w-screen">
       <div className="text-center text-white">
-        <h1 className="my-40 mb-36 text-8xl font-bold font-['Amatic_SC']">TALK TO ME</h1>    
-
-    <Button text="CONTINUE"/>
-
-        <Button text="CHAPTERS"/>
-        <Button text="START GAME IN ANOTHER LANGUAGE"/>
-        <Button text="START"/>
+        <h1 className="my-40 mb-36 text-8xl font-bold font-['Amatic_SC']">
+          TALK TO ME
+        </h1>
+        <div>
+          {story.length === 0 ? (
+            <Button text="START" />
+          ) : (
+            <>
+              <Button text="CONTINUE" />
+              <Button text="CHAPTERS" />
+              
+              <NavLink to={"/language"}>
+              <Button text="START GAME IN ANOTHER LANGUAGE" />
+              </NavLink>
+            </>
+          )}
+        </div>
       </div>
     </div>
-    )
-  }
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
