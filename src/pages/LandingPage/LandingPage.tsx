21d5a92fbadb261ad223a3b86b583582cf9fe6ff
Rafalzie <rafal19191996@gmail.com>
2024-12-02 15:24:17 +0000

    fixed Landing page button state

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,21 +1,28 @@
-import  Button from "../../components/button"
+import Button from "../../components/button";
 import { useState } from "react";
+import { useStory } from "../../providers/storyProvider";
 
 export function LandingPage() {
-  
-  
-    return (
-      
+  const { story } = useStory();
+
+  return (
     <div className="bg-gray-700 flex justify-center h-screen w-screen">
       <div className="text-center text-white">
-        <h1 className="my-40 mb-36 text-8xl font-bold font-['Amatic_SC']">TALK TO ME</h1>    
+        <h1 className="my-40 mb-36 text-8xl font-bold font-['Amatic_SC']">
+          TALK TO ME
+        </h1>
 
-    <Button text="CONTINUE"/>
+        {story.length === 0 ? (
+          <Button text="START" />
+        ) : (
+          <>
+            <Button text="CONTINUE" />
 
-        <Button text="CHAPTERS"/>
-        <Button text="START GAME IN ANOTHER LANGUAGE"/>
-        <Button text="START"/>
+            <Button text="CHAPTERS" />
+            <Button text="START GAME IN ANOTHER LANGUAGE" />
+          </>
+        )}
       </div>
     </div>
-    )
-  }
+  );
+}
