e4253dd9889f42b80efef92c92dc0b92fe927395
Rafalzie <rafal19191996@gmail.com>
2024-12-03 01:22:06 +0000

    Added Back Arrow to Landing Page
    Full screen resolution has to be fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,16 @@ import { useState } from "react";
 import "./App.css";
 import { textToSpeech } from "./utils/textToSpeech";
 
-
 import { StoryProvider } from "./providers/storyProvider";
 import { ChooseAdventureGame } from "./components/chooseAdventureGame";
-
+import { LandingPage } from "./Pages/LandingPage/LandingPage";
 
 function App() {
-
-
   return (
-
     <StoryProvider>
-      <ChooseAdventureGame />
-
+      <LandingPage />
+    </StoryProvider>
   );
 }
 
 export default App;
-
-  
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,21 +1,30 @@
-import  Button from "../../components/button"
-import { useState } from "react";
+import Button from "../../components/button";
+import { useStory } from "../../providers/storyProvider";
+import BackArrow from "../../components/BackArrow";
 
 export function LandingPage() {
-  
-  
-    return (
-      
-    <div className="bg-gray-700 flex justify-center h-screen w-screen">
+  const { story } = useStory();
+
+  return (
+    <div className="bg-gray-700 flex justify-center items-center h-screen w-screen relative">
+      {/* BackArrow positioned in the top-left corner */}
+      <BackArrow onClick={() => console.log("Back to previous page")} />
+
       <div className="text-center text-white">
-        <h1 className="my-40 mb-36 text-8xl font-bold font-['Amatic_SC']">TALK TO ME</h1>    
+        <h1 className="my-40 mb-36 text-8xl font-bold font-['Amatic_SC']">
+          TALK TO ME
+        </h1>
 
-    <Button text="CONTINUE"/>
-
-        <Button text="CHAPTERS"/>
-        <Button text="START GAME IN ANOTHER LANGUAGE"/>
-        <Button text="START"/>
+        {story.length === 0 ? (
+          <Button text="START" />
+        ) : (
+          <>
+            <Button text="CONTINUE" />
+            <Button text="CHAPTERS" />
+            <Button text="START GAME IN ANOTHER LANGUAGE" />
+          </>
+        )}
       </div>
     </div>
-    )
-  }
+  );
+}
