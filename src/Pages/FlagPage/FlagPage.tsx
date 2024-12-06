54372e0e3a82a6bc9de2e55951b2e826fbe95a79
Evelina-Ok <142876802+Evelina-Ok@users.noreply.github.com>
2024-12-06 09:41:36 +0000

    #font

--- src/Pages/FlagPage/FlagPage.tsx
+++ src/Pages/FlagPage/FlagPage.tsx
@@ -26,7 +26,7 @@ export function FlagPage() {
     <>
       <BackArrow />
       <div className="min-h-screen w-dvw bg-gray-500 flex flex-col items-center px-4 py-8">
-        <h1 className="my-10 mb-4 text-3xl font-bold  text-white text-center font-['Amatic_SC']">
+        <h1 className="my-8 mb-3 text-4xl px-2 font-bold  text-white text-center font-['Jersey_10']">
           CHOOSE LANGUAGE YOU WANT TO LEARN
         </h1>
 
